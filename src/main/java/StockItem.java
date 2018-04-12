1435f043c407c6362f25633d545ba2f4a2e06449
DocLJN <ljnoble@gmail.com>
2018-04-12 10:57:30 +0100

    Stock Item returns Price

--- src/main/java/StockItem.java
+++ src/main/java/StockItem.java
@@ -1,6 +1,16 @@
 public enum StockItem {
 
-    A,
-    B,
-    C
+    A (65),
+    B (100),
+    C (150);
+
+    private final int price;   // in cents
+    StockItem(int price) {
+        this.price = price;
+    }
+    private int price() { return price; }
+
+    public int getPrice() {
+        return price();
+    }
 }
--- src/test/java/StockItemTest.java
+++ src/test/java/StockItemTest.java
@@ -3,12 +3,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class StockItemTypeTest {
+public class StockItemTest {
 
     StockItem stockItem;
 
     @Test
-    public void testTypeHasCost() {
-        assertEquals("A", stockItem.getType());
+    public void testItemReturnsPrice() {
+        assertEquals(65, stockItem.A.getPrice());
     }
 }
